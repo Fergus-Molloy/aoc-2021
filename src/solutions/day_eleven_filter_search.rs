620c4de26f2877bb3e97f635146dbdecafca74e5
Fergus Molloy <fergus@molloy.xyz>
2021-12-11 15:32:55 +0000

    day eleven fast

--- src/solutions/day_eleven_filter_search.rs
+++ src/solutions/day_eleven_filter_search.rs
@@ -1,5 +1,4 @@
 use crate::advent_of_code::AdventOfCodeInput;
-use std::cmp::Ordering;
 
 const MAX_X: usize = 9;
 const MAX_Y: usize = 9;
@@ -7,10 +6,9 @@ const MAX_Y: usize = 9;
 #[derive(Clone)]
 pub struct Octopi {
     octopi: Vec<Octopus>,
-    h_count: u32,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 pub struct Octopus {
     pos: Position,
     energy: u8,
@@ -66,40 +64,6 @@ impl Position {
     }
 }
 
-#[allow(clippy::if_same_then_else)]
-impl std::cmp::Ord for Position {
-    fn cmp(&self, other: &Position) -> Ordering {
-        if self.x == other.x && self.y == other.y {
-            Ordering::Equal
-        } else if self.x < other.x && self.y == other.y {
-            Ordering::Less
-        } else if self.x == other.x && self.y < other.y {
-            Ordering::Less
-        } else {
-            Ordering::Greater
-        }
-    }
-}
-impl std::cmp::PartialOrd for Position {
-    fn partial_cmp(&self, other: &Position) -> Option<Ordering> {
-        if self.x == other.x && self.y == other.y {
-            Some(Ordering::Equal)
-        } else if self.x < other.x && self.y == other.y {
-            Some(Ordering::Less)
-        } else if self.x == other.x && self.y < other.y {
-            Some(Ordering::Less)
-        } else {
-            Some(Ordering::Greater)
-        }
-    }
-}
-
-impl Octopi {
-    fn get(&mut self, pos: &Position) -> &mut Octopus {
-        &mut self.octopi[pos.x + (pos.y * (MAX_X + 1))]
-    }
-}
-
 impl Octopus {
     #[inline(always)]
     pub fn new(energy: u8, x: usize, y: usize) -> Self {
@@ -113,7 +77,6 @@ impl Octopus {
 
 pub fn parse(inp: String) -> Octopi {
     Octopi {
-        h_count: 0,
         octopi: inp
             .lines()
             .enumerate()
@@ -129,49 +92,48 @@ pub fn parse(inp: String) -> Octopi {
 
 pub fn solve(aoc_input: AdventOfCodeInput) -> String {
     let mut octopi = parse(aoc_input.inp);
-    octopi.octopi.sort_by(|a, b| a.pos.cmp(&b.pos));
     let pt1 = part_one(&mut octopi.clone());
     let pt2 = part_two(&mut octopi);
     format!("Day 1: ({},{})", pt1, pt2)
 }
 
-fn flash(s_octopi: &mut Octopi) {
-    let count = s_octopi.h_count;
-    let to_flash = s_octopi
-        .octopi
-        .iter_mut()
-        .filter(|x| x.energy > 9 && !x.highlighted);
+fn flash(octopi: &mut [Octopus]) {
+    let count = octopi.iter().filter(|x| x.highlighted).count();
+    let to_flash = octopi.iter_mut().filter(|x| x.energy > 9 && !x.highlighted);
     let mut adj = Vec::new();
     for o in to_flash {
         o.highlighted = true;
-        s_octopi.h_count += 1;
-        adj.append(&mut o.pos.get_adjacent());
+        for pos in o.pos.get_adjacent() {
+            adj.push(pos);
+        }
     }
     for pos in adj {
-        s_octopi.get(&pos).energy += 1;
+        for o in octopi.iter_mut().filter(|x| x.pos == pos) {
+            o.energy += 1;
+        }
     }
-    if s_octopi.h_count != count {
-        flash(s_octopi);
+    if octopi.iter().filter(|x| x.highlighted).count() != count {
+        flash(octopi);
     }
 }
 
-fn step(octopi: &mut Octopi) -> u32 {
-    for octopus in &mut octopi.octopi {
+fn step(octopi: &mut [Octopus]) -> u32 {
+    for octopus in octopi.iter_mut() {
         octopus.highlighted = false;
         octopus.energy += 1;
     }
-    octopi.h_count = 0;
     flash(octopi);
-    for o in octopi.octopi.iter_mut().filter(|x| x.highlighted) {
+    let highlighted_count = octopi.iter().filter(|x| x.highlighted).count();
+    for o in octopi.iter_mut().filter(|x| x.highlighted) {
         o.energy = 0;
     }
-    octopi.h_count
+    highlighted_count as u32
 }
 
 pub fn part_one(octopi: &mut Octopi) -> u32 {
     let mut total = 0;
     for _ in 0..100 {
-        total += step(octopi);
+        total += step(&mut octopi.octopi);
     }
     total
 }
@@ -179,11 +141,11 @@ pub fn part_one(octopi: &mut Octopi) -> u32 {
 pub fn part_two(octopi: &mut Octopi) -> u32 {
     let mut steps = 0;
     let mut all_flash = false;
-    let length = octopi.octopi.len() as u32;
+    let length = octopi.octopi.len();
     while !all_flash {
-        let highlighted = step(octopi);
+        let highlighted = step(&mut octopi.octopi);
 
-        all_flash = highlighted == length;
+        all_flash = highlighted == length as u32;
         steps += 1;
     }
     steps
@@ -196,14 +158,38 @@ mod test {
     #[test]
     fn d11a() {
         let aoc_input = AdventOfCodeInput::get_input(11);
-        let mut octopi = parse(aoc_input.inp);
+        let mut octopi = Octopi {
+            octopi: aoc_input
+                .inp
+                .lines()
+                .enumerate()
+                .flat_map(|(y, line)| {
+                    line.chars()
+                        .enumerate()
+                        .map(|(x, o)| Octopus::new(o.to_string().parse::<u8>().unwrap(), x, y))
+                        .collect::<Vec<Octopus>>()
+                })
+                .collect::<Vec<Octopus>>(),
+        };
         assert_eq!(part_one(&mut octopi), 1652);
     }
 
     #[test]
     fn d11b() {
         let aoc_input = AdventOfCodeInput::get_input(11);
-        let mut octopi = parse(aoc_input.inp);
+        let mut octopi = Octopi {
+            octopi: aoc_input
+                .inp
+                .lines()
+                .enumerate()
+                .flat_map(|(y, line)| {
+                    line.chars()
+                        .enumerate()
+                        .map(|(x, o)| Octopus::new(o.to_string().parse::<u8>().unwrap(), x, y))
+                        .collect::<Vec<Octopus>>()
+                })
+                .collect::<Vec<Octopus>>(),
+        };
         assert_eq!(part_two(&mut octopi), 220);
     }
 }
